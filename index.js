d8d4868e5f5f66a2033be8187b8cf83dccd138dc
kizya007 <kizya007@mail.ru>
2023-01-31 22:07:02 +0300

    javascript

--- index.js
+++ index.js
@@ -10,5 +10,45 @@ document.addEventListener('DOMContentLoaded', function () {
             mobilemenu.classList.add('active');
         }
     })
+    const getStartedButton = document.querySelector('.header-section-center-button-left');
+    function addClass() {
+
+        const getTryFoFreeButton = document.querySelector('.header-section-center-button-right');
+        console.log(getStartedButton.innerHTML)
+        if (getTryFoFreeButton.classList.contains('no-showed')) {
+            getTryFoFreeButton.classList.remove('no-showed')
+            getStartedButton.innerHTML = 'get started'
+        }
+        else {
+            getTryFoFreeButton.classList.add('no-showed')
+            getStartedButton.innerHTML = 'add no showed'
+        }
+
+
+    }
+    getStartedButton.addEventListener('click', addClass)
+    const signUpButton = document.querySelector('.header-section-container-button');
+    const modal = document.querySelector('.modal-window');
+    const modalCloseButton = document.querySelector('.modal-window-button-close');
+    const subscribeButton = document.querySelector('.section-subscribe-footer-button');
+    signUpButton.addEventListener('click', function () {
+        console.log(7)
+        if (!modal.classList.contains('showed')) {
+            modal.classList.add('showed')
+        }
+    })
+    modalCloseButton.addEventListener('click', function () {
+        modal.classList.remove('showed')
+    })
+    subscribeButton.addEventListener('click', function(event){
+        console.log(event)
+        subscribeButton.style.backgroundColor = 'red'
+        if(subscribeButton.style.backgroundColor === 'red'){
+            subscribeButton.style.backgroundColor = 'orange'
+            
+        }
+        
+  
+    })
 
 });
